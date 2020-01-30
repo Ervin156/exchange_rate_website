54e7d4f14395ae2bcf44d6a07a8b430ebebc7bce
Ervin156 <dmitriylystov@gmail.com>
2020-01-30 20:28:31 +0200

    create build folder

---
+++ build/precache-manifest.c7146068419c0524ebaef57b22a85385.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81338ddae800a4da527f954eb77f6903",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc491e7ff51b8c115ab2",
+    "url": "/static/css/main.13e00e4e.chunk.css"
+  },
+  {
+    "revision": "fc43ddcb45a32b6cfddf",
+    "url": "/static/js/2.a83409a8.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.a83409a8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fc491e7ff51b8c115ab2",
+    "url": "/static/js/main.652451a1.chunk.js"
+  },
+  {
+    "revision": "fb17c763c60011bbad89",
+    "url": "/static/js/runtime-main.f194770d.js"
+  }
+]);
