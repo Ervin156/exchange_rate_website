f903437d40c4138770a3576f3bd9de35f66469ae
Ervin156 <dmitriylystov@gmail.com>
2020-02-06 10:55:57 +0200

    adaptation added

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Header from './Header/Header';
 import Main from './Main/Main';
 import Footer from './Footer/Footer';
-import './App.css';
+import './App.scss';
 
 
 class App extends React.Component {
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -6,23 +6,23 @@ class Nav extends React.Component {
 
     render() {
         return (
-            <div className='nav'>
                 <div className="header-nav">
                     <div className="container">
                         <nav>
-                            <ul>
-                            
-                                <li><NavLink to="/">Курс Доллара</NavLink></li>
-                                <li><NavLink to="/rateUAH">Курс гривны</NavLink></li>
-                                <li><NavLink to="/points">Пункты обмена</NavLink></li>
-                                <li><NavLink to="/photos">Фоточки</NavLink></li>
-                                <li><NavLink to="/contacts">Контакты</NavLink></li>
-
-                            </ul>
+                            <input type="checkbox" id="checkbox-menu" />
+                            <label htmlFor="checkbox-menu">
+                                <ul className="menu touch">
+                                    <li><NavLink to="/">Курс Доллара</NavLink></li>
+                                    <li><NavLink to="/rateUAH">Курс гривны</NavLink></li>
+                                    <li><NavLink to="/points">Пункты обмена</NavLink></li>
+                                    <li><NavLink to="/photos">Фоточки</NavLink></li>
+                                    <li><NavLink to="/contacts">Контакты</NavLink></li>
+                                </ul>
+                                <span className="toggle">=</span>
+                            </label>
                         </nav>
                     </div>
                 </div>
-            </div>
         )
     }
 }
